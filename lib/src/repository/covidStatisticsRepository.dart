59cc5586cf97f07dfbd3130ecdc4e026b86238d6
mike.bskim <75249625+mike-bskim@users.noreply.github.com>
2021-08-04 23:42:17 +0200

    Update covidStatisticsRepository.dart

--- lib/src/repository/covidStatisticsRepository.dart
+++ lib/src/repository/covidStatisticsRepository.dart
@@ -11,7 +11,7 @@ class CovidStatisticsRepository {
         baseUrl: "http://openapi.data.go.kr",
         queryParameters: {
           'ServiceKey':
-          '',
+          '개인키',
         },
       ),
     );
